6cc3b31dc4ed461acea5f7f603ad7836bcfcfced
Chase Sarif <49916173+chaseSarif@users.noreply.github.com>
2019-05-01 13:34:00 -0400

    Add files via upload

---
+++ NumberLvl1.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class NumberLvl1 {
+	
+	int guess;
+	int answer;
+	int range;
+	int max;
+	
+	public static void main(String args[]) {
+		int guess = 0;
+		int max = 0;
+		
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter your max value");
+		max = reader.nextInt();
+		
+		double randomDouble = Math.random();
+		randomDouble = randomDouble * max + 1;
+		int answer = (int) randomDouble;
+		System.out.println(answer);
+		
+		System.out.println("Enter your guess!: ");
+		guess = reader.nextInt();
+		
+		while(guess!= answer) {
+			
+			if(guess < answer) {
+				System.out.println("Hint: Try Higer");
+				
+			}
+				
+			if(guess > answer) {
+				System.out.println("Hint: Try Lower");
+			}
+			
+			System.out.println("Guess again!: ");
+			guess = reader.nextInt();
+			
+			}
+		
+		System.out.println("Correct Answer! Play Again?");
+		
+		
+	}
+	
+	
+}
