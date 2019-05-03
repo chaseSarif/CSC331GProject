217016d6a0e2ab757a021bf0243049521b2b9d77
Chase Sarif <49916173+chaseSarif@users.noreply.github.com>
2019-05-03 17:38:20 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class Player {
     String pName;
+    Long gameTimePlayed = long(0);
     ArrayList<Levels> levelsList = new ArrayList<Levels>();
     ArrayList<oneGame> oneGameList = new ArrayList<oneGame>();
 
@@ -20,6 +21,27 @@ public class Player {
     public String getPName(){
         return this.pName;
     }
+    
+    public void addGamePlayTime(long newGame){
+        gameTimePlayed += newGame;
+    }
+    
+    public void setGamePlayTime(long gameTimePlay){
+        gameTimePlayed += gameTimePlay;
+    }
+    
+    public String getGamePlayTime(){
+        return this.gameTimePlayed.toString();
+    }    
+    
+    public void setOneGameList(ArrayList<oneGame> newList){
+        this.oneGameList = newList;
+    }
+    
+    public void addLvl(Levels newLevelObject){
+        this.levelsList.add(newLevelObject)
+    }        
+        
 
     public void setOneGameList(ArrayList<oneGame> newList){
         this.oneGameList = newList;
